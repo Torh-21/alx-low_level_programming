d8f6e2f26849f30a3dbd6c565f8d7e12a3b2536b
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-17 05:32:47 -0700

    Hot fix to the Hello, puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-/*
- *A simple Hello,puts program
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
