f1628dbea45e9c28ba8f148f1e7628b0aa9f3045
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-17 10:24:05 -0700

    Last changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types on the computer used for compilation and execution
+ * main - Prints the size of various types
  *
  * Return: 0 (Success)
  */
