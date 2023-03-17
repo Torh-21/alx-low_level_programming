b557c064dfd742b36c70fa360e74221c7fd981b2
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-17 10:17:50 -0700

    Changes to the 6-size.c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#inlcude <stdio.h>
 
 /**
- * main - Prints the size of various types
+ * main - Prints the size of various types on the computer used for compilation and execution
  *
  * Return: 0 (Success)
  */
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
