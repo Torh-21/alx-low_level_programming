9e04e20bd795017a7c8aa03e56d1b11e6daaedf6
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-23 07:55:32 -0700

    The 0-main.c file  is added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - tests if the function positive_or_negative()
+ * gives the correct output with an input of 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This is the README file for the debugging project
+The 0-main.c checks if the positive_or_negative function gives the correct output with a 0 input
