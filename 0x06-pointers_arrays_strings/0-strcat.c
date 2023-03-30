a6cd4205a08d51099f4d73d46506d074221086df
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-30 16:46:06 -0700

    The 0-strcat.c function is added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - Concatenates two strings
+ *
+ * @dest: input pointer string
+ * @src: input pointer string
+ *
+ * Return: character pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] !=  '\0'; i++)
+	{
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is the Readme file for the 0x06-pointers_arrays_strings directory
+The 0-strcat.c function concatenates two strings.
