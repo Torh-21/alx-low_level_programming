9b1dccd5741edf47a749d0f11e4fdde45fc52ec1
Majaro Toheeb <majarotoheeb@gmail.com>
2023-03-30 18:44:32 -0700

    The 2-strncpy.c function is added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: input char
+ * @src: input char
+ * @n: input specifier
+ *
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	
+	if (n > j)
+	{
+		dest[j] = '\0';
+	}
+
+	return (dest);
+}
