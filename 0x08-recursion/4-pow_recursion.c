2326dbbddcc76016a01171a00bf015ebaf5b8d5d
Majaro Toheeb <majarotoheeb@gmail.com>
2023-04-06 16:20:32 -0700

    The 4-pow_recursion.c function is added

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised
+ * to the power of y
+ *
+ * @x: integer argument
+ * @y: integer argument
+ *
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	int total;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+
+	total = x * _pow_recursion(x, (y - 1));
+	return (total);
+}
