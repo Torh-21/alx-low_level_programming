db33e6a948233402878bc9ac4d6dec161bb62177
Majaro Toheeb <majarotoheeb@gmail.com>
2023-04-11 18:29:21 -0700

    The 1-strdup.c funtion is added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string argument
+ *
+ * @str: character argument
+ *
+ * Return: character pointer
+ */
+char *_strdup(char *str)
+{
+	char *x;
+	int a, b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+
+	x = malloc(sizeof(char) * a);
+
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+
+	for (b = 0; b < a; b++)
+	{
+		x[b] = str[b];
+	}
+
+	return (x);
+
+	free(x);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 This is the README file for the 0x0B-malloc_free directory.
 The 0-create_array.c function creates an array of chars, and initializes it with a specific char.
+The 1-strdup.c function returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
