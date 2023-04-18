fbd6e8b70563af22ab70b5b8b3941a569bff9f60
Majaro Toheeb <majarotoheeb@gmail.com>
2023-04-17 18:31:54 -0700

    The dog.h struct is added

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This is the README file for the 0x0E-structures_typedef directory.
+The dog.h file is a struct type containing re-usable variables.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - a struct type containing re-usable arguments
+ *
+ * @name: character argument
+ * @age: float argument
+ * @owner: character argument
+ *
+ * Description: A struct named dog which holds three
+ * arguments of different types
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG */
