c720d148be9f4c5840467bc21eae7ff6bc6f5ff4
Majaro Toheeb <majarotoheeb@gmail.com>
2023-04-20 04:45:36 -0700

    Edge case fix to the 1-array_iterator.c file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (x = 0; x < size; x++)
 		action(array[x]);
 }
