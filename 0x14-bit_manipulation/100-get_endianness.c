47e398e6d80870b54382888c428875d4b4a0e0ef
Torh-21 <majarotoheeb@gmail.com>
2023-05-04 20:03:17 -0700

    The 100-get_endianness.c file is added

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 for big endian or 1 for little endian
+ */
+int get_endianness(void)
+{
+	int x;
+	char *a;
+
+	x = 1;
+	a = (char *) &x;
+
+	return (*a == 1);
+}
