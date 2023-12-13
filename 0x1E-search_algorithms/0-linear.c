a8788c2f9dd5f9cb6dfe996beefce9f2ea76d3ec
Torh-21 <majarotoheeb@gmail.com>
2023-12-12 16:22:04 -0800

    Search Algorithms started

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ * 
+ * @array: input array to be searched
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+	if (!array || size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value at index[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
