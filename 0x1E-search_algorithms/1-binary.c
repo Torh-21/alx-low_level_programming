10900adc313b76a8bff38ec556966c9d0467afc3
Torh-21 <majarotoheeb@gmail.com>
2023-12-12 16:37:46 -0800

    Binary search task added

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ * @array: input array to search the value in
+ * @size: size of the array
+ * @value: value to look for
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t i, mid;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
